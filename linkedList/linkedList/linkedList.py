1a133746ed85d886e566aa4c29975caf46ad995e
Husam-Zabian <husamthubian99@yahoo.com>
2023-05-26 18:02:50 +0000

    CC07 Done

--- linkedList/linkedList/linkedList.py
+++ linkedList/linkedList/linkedList.py
@@ -49,3 +49,87 @@ class LinkedList:
             current_node = current_node.next
         result += "NULL"
         return result
+    def append(self, value):
+        """
+        Append a new node with the given value to the end of the linked list.
+        Args:
+            value: The value to be stored in the new node.
+        """
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+            return
+        current_node = self.head
+        while current_node.next:
+            current_node = current_node.next
+        current_node.next = new_node
+
+    def insert_before(self, value, new_value):
+        """
+        Insert a new node with the given new value before the node with the given value in the linked list.
+        Args:
+            value: The value of the node before which the new node should be inserted.
+            new_value: The value to be stored in the new node.
+        """
+        new_node = Node(new_value)
+        if self.head == None:
+            return
+        if self.head.value == value:
+            new_node.next = self.head
+            self.head = new_node
+            return
+        current_node = self.head
+        while current_node.next:
+            if current_node.next.value == value:
+                new_node.next = current_node.next
+                current_node.next = new_node
+                return
+            current_node = current_node.next
+        
+
+    def insert_after(self, value, new_value):
+        """
+        Insert a new node with the given new value after the node with the given value in the linked list.
+        Args:
+            value: The value of the node after which the new node should be inserted.
+            new_value: The value to be stored in the new node.
+        """
+        new_node = Node(new_value)
+        if self.head == None:
+            return
+        current_node = self.head
+        while current_node:
+            if current_node.value == value:
+                new_node.next = current_node.next
+                current_node.next = new_node
+                return
+            current_node = current_node.next
+
+    def kthFromEnd(self,k):
+        """
+        Returns the value of the node that is k places from the tail of the linked list.
+        
+        Args:
+            k (int): The distance from the end of the linked list.
+        """
+        if k < 0:
+            raise ValueError("k cannot be negative")
+    
+        if self.head is None:
+            return None
+        
+        current_node = self.head
+        length = 0
+        while current_node:
+            length += 1
+            current_node = current_node.next
+        
+        if k >= length:
+            return None
+        
+        current_node = self.head
+        for _ in range(1,length - k):
+            current_node = current_node.next
+            
+        
+        return current_node.value 
